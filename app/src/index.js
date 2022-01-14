996f0b51068e97a65cf2149077c80a8ba3aa3ea7
flexiconn <simoncox2003@gmail.com>
2022-01-14 12:31:32 +0100

    import fixed

--- app/src/index.js
+++ app/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import store from "./Redux/store"
-import { CookiesProvider } from 'react-cookie';
+
 
 document.title = "test"
 
