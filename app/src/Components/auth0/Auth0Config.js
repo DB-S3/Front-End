c6bb60c0adf001ca3603bdfb43b768a28fff5c25
flexiconn <simoncox2003@gmail.com>
2022-01-14 13:00:05 +0100

    new callback

--- app/src/Components/auth0/Auth0Config.js
+++ app/src/Components/auth0/Auth0Config.js
@@ -1,7 +1,7 @@
 export const AUTH_CONFIG = {
     domain: 'ortisy.eu.auth0.com',
     clientID: 'hR67oZKxkx2eJwBFGP82Bsm7q0i3KAGM',
-    redirectUri: 'http://localhost:3000/startSession',
+    redirectUri: 'https://jolly-swartz-d50d33.netlify.app/startSession',
     audience: 'https://ortisy.com'
 };
 
